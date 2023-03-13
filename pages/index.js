757855ce1b392c6ceb5f519191922d32f40e7634
MohsenSami <mohsensami88@gmail.com>
2023-03-13 22:12:18 +0330

    Navigation add in Header component

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <Header />
         </>
     );
 }
