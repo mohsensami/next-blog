b6de4722f06b3999bd94640a63a434385f3d7a00
MohsenSami <mohsensami88@gmail.com>
2023-03-13 23:01:11 +0330

    section1 added

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Format from "@/layout/format";
+import Section1 from "@/components/section1";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
         <Format>
-            <h1>Main content</h1>
+            <Section1></Section1>
         </Format>
     );
 }
