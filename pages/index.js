995ed631da0fb8d3b91c942d166ad79f9517c19a
MohsenSami <mohsensami88@gmail.com>
2023-03-14 22:53:59 +0330

    test2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Format from "@/layout/format";
+import Section1 from "@/components/section1";
+import Section2 from "@/components/section2";
+import Section3 from "@/components/section3";
+import Section4 from "@/components/section4";
 
-function index() {
-    return <div>index</div>;
+// const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+    return (
+        <Format>
+            <Section1></Section1>
+            <Section2></Section2>
+            <Section3></Section3>
+            <Section4></Section4>
+        </Format>
+    );
 }
-
-export default index;
