7c5ba85d6c192c6abdfcbb8ba2d95cd4329073c8
MohsenSami <mohsensami88@gmail.com>
2023-03-14 11:05:59 +0330

    newsletter component added andfooter component updated

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
 import Link from "next/link";
-// import Newslatter from "./_child/newslatter";
+import Newslatter from "./_child/newsletter";
 
 export default function footer() {
     const bg = {
@@ -11,22 +11,28 @@ export default function footer() {
 
     return (
         <footer className="bg-gray-50" style={bg}>
-            {/* <Newslatter></Newslatter> */}
+            <Newslatter></Newslatter>
             <div className="container mx-auto flex justify-center py-12">
                 <div className="py-5">
                     <div className="flex gap-6 justify-center">
-                        <Link href={"/"}>
-                            <ImFacebook color="#888888" />
+                        <Link legacyBehavior href={"/"}>
+                            <a>
+                                <ImFacebook color="#888888" />
+                            </a>
                         </Link>
-                        <Link href={"/"}>
-                            <ImTwitter color="#888888" />
+                        <Link legacyBehavior href={"/"}>
+                            <a>
+                                <ImTwitter color="#888888" />
+                            </a>
                         </Link>
-                        <Link href={"/"}>
-                            <ImYoutube color="#888888" />
+                        <Link legacyBehavior href={"/"}>
+                            <a>
+                                <ImYoutube color="#888888" />
+                            </a>
                         </Link>
                     </div>
 
-                    <p className="py-5 text-gray-400">Copyright ©2023 All rights reserved | This template is made with by mSami</p>
+                    <p className="py-5 text-gray-400">Copyright ©2022 All rights reserved | This template is made with by Daily Tuition</p>
                     <p className="text-gray-400 text-center">Terms & Condition</p>
                 </div>
             </div>
